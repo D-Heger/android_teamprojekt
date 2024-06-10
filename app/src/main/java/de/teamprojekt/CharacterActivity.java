d309daa44251bb1464a92fc5bfa0a62170c76fbd
David Heger <david_heger@hotmail.de>
2024-06-10 19:45:04 +0200

    fix styling, add options menu

--- app/src/main/java/de/teamprojekt/CharacterActivity.java
+++ app/src/main/java/de/teamprojekt/CharacterActivity.java
@@ -2,7 +2,10 @@ package de.teamprojekt;
 
 import static de.teamprojekt.Utils.setNavBar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +21,28 @@ public class CharacterActivity extends AppCompatActivity {
         // Set up bottom navigation bar
         BottomNavigationView bnView = findViewById(R.id.bottom_navigation);
         setNavBar(bnView, this, R.id.navigation_character);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(CharacterActivity.this, SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_reset) {
+            startActivity(new Intent(CharacterActivity.this, ResetActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/de/teamprojekt/DetailActivity.java
+++ app/src/main/java/de/teamprojekt/DetailActivity.java
@@ -1,4 +1,47 @@
 package de.teamprojekt;
 
-public class DetailActivity {
+import static de.teamprojekt.Utils.setNavBar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Set up bottom navigation bar
+        BottomNavigationView bnView = findViewById(R.id.bottom_navigation);
+        setNavBar(bnView, this, R.id.navigation_add);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(DetailActivity.this, SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_reset) {
+            startActivity(new Intent(DetailActivity.this, ResetActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
