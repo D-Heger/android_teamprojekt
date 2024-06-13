cd8fda00e5e2e98360e4eccc527b1f0ec5aa6b71
Thor Aron Diedrich <thor.a.diedrich@hochschule-stralsund.de>
2024-06-13 21:18:54 +0200

    [WIP] calculateLevel

--- app/src/main/java/de/teamprojekt/Util/Constants/CalculationConstants.java
+++ app/src/main/java/de/teamprojekt/Util/Constants/CalculationConstants.java
@@ -4,5 +4,8 @@ public class CalculationConstants {
     public static final int INITIAL_EXP = 100;
     public static final int BASE_EXP = 50;
     public static final int EXP_MUL = 2;
+    public static final int EXP_LOW_PRIORITY = 1;
+    public static final int EXP_MEDIUM_PRIORITY = 2;
+    public static final int EXP_HIGH_PRIORITY = 3;
 
 }
--- app/src/main/java/de/teamprojekt/Util/LevelCalculator.java
+++ app/src/main/java/de/teamprojekt/Util/LevelCalculator.java
@@ -1,8 +1,15 @@
 package de.teamprojekt.Util;
 
 import static de.teamprojekt.Util.Constants.CalculationConstants.BASE_EXP;
+import static de.teamprojekt.Util.Constants.CalculationConstants.EXP_HIGH_PRIORITY;
+import static de.teamprojekt.Util.Constants.CalculationConstants.EXP_LOW_PRIORITY;
+import static de.teamprojekt.Util.Constants.CalculationConstants.EXP_MEDIUM_PRIORITY;
 import static de.teamprojekt.Util.Constants.CalculationConstants.EXP_MUL;
 import static de.teamprojekt.Util.Constants.CalculationConstants.INITIAL_EXP;
+
+import de.teamprojekt.Entity.Character;
+import de.teamprojekt.Entity.Enum.Skill;
+import de.teamprojekt.Entity.Todo;
 
 public class LevelCalculator {
     public static int calculateLevelCost(int level) {
@@ -12,6 +19,42 @@ public class LevelCalculator {
     public static int calculateProgress(int level, int exp) {
         int levelCost = calculateLevelCost(level);
         return (int) Math.floor((double) (exp * 100) / levelCost);
+    }
+
+    public static Character calculateLevel(Todo todo, Character character) {
+        Skill type = todo.getCategory().getSkill();
+        int exp = character.getExperience();
+        switch (todo.getPriority()) {
+            case LOW:
+                exp = exp * EXP_LOW_PRIORITY;
+                break;
+            case MEDIUM:
+                exp = exp * EXP_MEDIUM_PRIORITY;
+                break;
+            case HIGH:
+                exp = exp * EXP_HIGH_PRIORITY;
+                break;
+        }
+
+        switch (type) {
+            case STRENGTH:
+                character.setStrengthExp(character.getStrengthExp() + exp);
+                break;
+            case PERCEPTION:
+                break;
+            case ENDURANCE:
+                break;
+            case CHARISMA:
+                break;
+            case INTELLIGENCE:
+                break;
+            case AGILITY:
+                break;
+            case LUCK:
+                break;
+        }
+
+        return character;
     }
 
 }
